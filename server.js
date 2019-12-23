f2280caded3228037fe34dc2871023024b6938d1
James Miller <james.rex.miller.4@gmail.com>
2019-12-22 21:45:22 -0700

    Refactor callbacks with promises

--- server.js
+++ server.js
@@ -27,23 +27,34 @@ app.get('/messages', (req, res) => {
 app.post('/messages', (req, res) => {
   var message = new Message(req.body)
 
-  message.save((err) => {
-    if (err)
-      res.sendStatus(500);
-    
-    Message.find({message: 'badword'}, (err, censored) => {
+  message.save().then(() => {
+    console.log('saved')
+    return Message.findOne({message: 'badword'})
+  })
+    .then(censored => {
       if (censored) {
         console.log('censored words found', censored)
-        Message.remove({_id: censored.id}, (err) => {
-          console.log('removed censored message')
-        });
+        return Message.deleteOne({_id: censored.id})
       }
+      io.emit('message', req.body)
+      res.sendStatus(200)
+    })
+    .catch((err) => {
+      res.sendStatus(500)
+      return console.error(err)
     });
-    io.emit('message', req.body);
-    res.sendStatus(200);
-  });
 });
 
+// Message.findOne({message: 'badword'}, (err, censored) => {
+//   if (censored) {
+//     console.log('censored words found', censored)
+//     Message.remove({_id: censored.id}, (err) => {
+//       console.log('removed censored message')
+//     });
+//   }
+// });
+// io.emit('message', req.body);
+// res.sendStatus(200);
 io.on('connection', (socket) => {
   console.log('a user connected');
 });
