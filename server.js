e5b04bd2e30afee754cfbb789abc22c4a7640239
James Miller <james.rex.miller.4@gmail.com>
2019-12-22 21:29:32 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const mongoose = require('mongoose');
+
+
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const dbUrl = "mongodb+srv://user:Tw73nGKnCSafrw4P@cluster0-bvznq.mongodb.net/test?retryWrites=true&w=majority"
+
+const Message = mongoose.model('Message', {
+  name: String,
+  message: String
+});
+
+
+app.get('/messages', (req, res) => {
+  Message.find({}, (err, messages) => {
+    res.send(messages)
+  })
+});
+
+app.post('/messages', (req, res) => {
+  var message = new Message(req.body)
+
+  message.save((err) => {
+    if (err)
+      res.sendStatus(500);
+    
+    Message.find({message: 'badword'}, (err, censored) => {
+      if (censored) {
+        console.log('censored words found', censored)
+        Message.remove({_id: censored.id}, (err) => {
+          console.log('removed censored message')
+        });
+      }
+    });
+    io.emit('message', req.body);
+    res.sendStatus(200);
+  });
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+mongoose.connect(dbUrl, (err) => {
+  console.log('mongo db connection', err)
+})
+const server = http.listen(3002, () => {
+  console.log('server is listening on PORT', server.address().port);
+});
