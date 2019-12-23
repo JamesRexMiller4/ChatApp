7822b93cec94311c4250f8c450991bf37ffbf96d
James Miller <james.rex.miller.4@gmail.com>
2019-12-22 22:29:42 -0700

    Add get a user's messages functionality and write test

--- spec/server.spec.js
+++ spec/server.spec.js
@@ -1,17 +1,34 @@
+/* eslint-disable no-undef */
 const request = require('request');
 
 describe('get messages', () => {
   it('should return 200 ok', (done) => {
     request.get('http://localhost:3002/messages', (err, res) => {
-      expect(res.statusCode).toEqual(200)
-      done()
-    })
-  })
+      expect(res.statusCode).toEqual(200);
+      done();
+    });
+  });
 
   it('should return an array with messages', (done) => {
     request.get('http://localhost:3002/messages', (err, res) => {
-      expect(JSON.parse(res.body).length).toBeGreaterThan(0)
-      done()
-    })
-  })
-})
+      expect(JSON.parse(res.body).length).toBeGreaterThan(0);
+      done();
+    });
+  });
+
+  describe('get messages from specific user', () => {
+    it('should return 200 ok', (done) => {
+      request.get('http://localhost:3002/messages/Tim', (err, res) => {
+        expect(res.statusCode).toEqual(200);
+        done();
+      });
+    });
+
+    it('name should be tim', (done) => {
+      request.get('http://localhost:3002/messages/Tim', (err, res) => {
+        expect(JSON.parse(res.body)[0].name).toEqual('Tim');
+        done();
+      });
+    });
+  });
+});
